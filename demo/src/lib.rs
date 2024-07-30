a16a4be23d4bb6dcb07b9b3b7ab51096ac9ee222
Shane F. Carr <shane@unicode.org>
2024-07-30 16:47:36 -0700

    Remove dependency on icu_experimental

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,13 +1,25 @@
 extern crate alloc;
 
+mod structs;
+
 pub mod provider {
     pub struct Baked;
+
 
     const _: () = {
         use experimental_data::*;
         pub mod icu {
-            pub use icu_experimental as experimental;
             pub use experimental_data::icu_locale as locale;
+
+            pub mod experimental {
+                pub mod dimension {
+                    pub mod provider {
+                        pub mod units {
+                            pub use crate::structs::*;
+                        }
+                    }
+                }
+            }
         }
         make_provider!(Baked);
 
