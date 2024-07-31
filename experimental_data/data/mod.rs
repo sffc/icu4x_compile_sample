fcf1e4b94cfad683e4328162ad56a4909c80270e
Shane F. Carr <shane@unicode.org>
2024-07-30 17:51:00 -0700

    include_bytes! (trie only)

--- experimental_data/data/mod.rs
+++ experimental_data/data/mod.rs
@@ -3,7 +3,8 @@
 // include!("units_display_name_v1_marker.rs.data");
 // include!("imports.rs.data");
 // include!("constconstruct.rs.data");
-include!("helper.rs.data");
+// include!("helper.rs.data");
+include!("includebytes.rs.data");
 
 /// Marks a type as a data provider. You can then use macros like
 /// `impl_core_helloworld_v1` to add implementations.
